58af1fa1ea1fde67e3ab2afc37be6893d7932c39
Faiz334 <olfaiz@hotmail.fr>
2022-04-11 19:48:16 +0200

    route home

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -56,7 +56,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Aller sur le site', 'fa fa-undo', 'app_home');
+        yield MenuItem::linkToRoute('Aller sur le site', 'fa fa-undo', 'app_home');
 
         yield MenuItem::subMenu('Formations', 'fas fa-graduation-cap',)->setSubItems([
             MenuItem::linkToCrud('Toutes les formations','fas fa-graduation-cap', Formation::class),
