8f6904b74ac34b84ef494608d07405453b723cc6
Faiz334 <olfaiz@hotmail.fr>
2022-12-13 16:53:49 +0100

    v4

--- src/Form/InstructorType.php
+++ src/Form/InstructorType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -21,7 +22,7 @@ class InstructorType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('username')
-            ->add('description')
+            ->add('description', TextareaType::class)
              ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
